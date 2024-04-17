5a41e3eb3b208f2d2e6f8c7c38a356f87d82e17d
axsxpro <cmptpro@outlook.com>
2024-04-18 00:40:44 +0200

    maj placeholder

--- src/app/epargne-form/epargne-form.component.ts
+++ src/app/epargne-form/epargne-form.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-epargne-form',
@@ -9,29 +9,40 @@ import { FormGroup, FormControl } from '@angular/forms';
 
 export class EpargneFormComponent {
 
-// initialisation du formulaire 
-epargneForm: FormGroup = new FormGroup({
+  // initialisation du formulaire 
+  epargneForm: FormGroup = new FormGroup({
+
     capital: new FormControl(),
-    savingsAmount: new FormControl(),
+    savingsAmount: new FormControl('', [Validators.min(0)]), //[Validators.min(0)]:  la valeur saisie dans le champ du montant épargné est supérieure ou égale à zéro.
     month: new FormControl(),
     years: new FormControl(),
     savingsDuration: new FormControl()
+
   });
 
 
+  updateMinEpargne(event: any) {
+    
+    // Extraire la valeur de l'option sélectionnée
+    const selectedMontant = event.target.value; 
 
-  updatePlaceholder(event: any) {
+    // modification de la valeur de l'input
+    this.epargneForm.controls['capital'].setValue(null); // Réinitialiser la valeur de l'input à vide
+    this.epargneForm.controls['capital'].setValidators([Validators.min(selectedMontant)]);// modification de la contrainte minimale de l'input en fonction de l'option
+    //this.epargneForm.controls['capital'].updateValueAndValidity()
 
-  const selectedLivret = event.target.value;
-  this.epargneForm.controls['capital'].setValidators([Validators.min(this.livretMontantInitial[selectedLivret])]);
-  this.epargneForm.controls['capital'].updateValueAndValidity();
-  
-}
+    // Modifier le placeholder en fonction de la valeur sélectionnée
+    const inputElement = document.getElementById('inputCapital');
+    if (inputElement) {
+      inputElement.setAttribute('placeholder', 'Capital minimum de : ' + selectedMontant + '€');
+    } 
+
+  }
+
 
 
   // Méthode appelée lors de la soumission du formulaire
   calculateEpargne() {
-  
 
   }
 
