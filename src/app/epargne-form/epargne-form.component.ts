fe95e42b0fa9bac28015f8279d5242e4fb3feb4d
axsxpro <cmptpro@outlook.com>
2024-04-17 14:54:11 +0200

    maj form

--- src/app/epargne-form/epargne-form.component.ts
+++ src/app/epargne-form/epargne-form.component.ts
@@ -1,10 +1,41 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
-  selector: 'app-epargne-form', // selecteur
+  selector: 'app-epargne-form',
   templateUrl: './epargne-form.component.html',
-  styleUrl: './epargne-form.component.css'
+  styleUrls: ['./epargne-form.component.css']
 })
+
 export class EpargneFormComponent {
 
+// initialisation du formulaire 
+epargneForm: FormGroup = new FormGroup({
+    capital: new FormControl(),
+    savingsAmount: new FormControl(),
+    month: new FormControl(),
+    years: new FormControl(),
+    savingsDuration: new FormControl()
+  });
+
+
+
+  updatePlaceholder(event: any) {
+
+  const selectedLivret = event.target.value;
+  this.epargneForm.controls['capital'].setValidators([Validators.min(this.livretMontantInitial[selectedLivret])]);
+  this.epargneForm.controls['capital'].updateValueAndValidity();
+  
 }
+
+
+  // Méthode appelée lors de la soumission du formulaire
+  calculateEpargne() {
+  
+
+  }
+
+
+
+}
+
